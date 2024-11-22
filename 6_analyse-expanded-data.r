7a759776decbb570cd4d22a2577b7d1e04a813dd
jayruffellvw <152837969+jayruffellvw@users.noreply.github.com>
2024-11-22 13:19:00 +1300

    update gitignore plus add OVERALL values to charts.

--- 6_analyse-expanded-data.r
+++ 6_analyse-expanded-data.r
@@ -12,35 +12,68 @@ sql_aggregated <- sql_expanded %>%
     summarise(session_count = length(session_result))
 
 # check what the trends are in the raw data
-sql_expanded %>%
+p1 <- sql_expanded %>%
     sample_n(50000) %>%
     ggplot(aes(date_id, session_result, colour = group_id)) +
     geom_jitter(alpha = 0.3, position = position_jitterdodge(jitter.height = 0.1)) + 
     geom_smooth()
+# print(p1)
 
 # and in the aggregated data
 conv_rate_data <- sql_aggregated %>%
     pivot_wider(names_from = session_result, values_from = session_count) %>%
     transmute(date_id, group_id, n = `0` + `1`, conversions = `1`, conversion_rate = `1` / (`0` + `1`))
-conv_rate_data %>%
-ggplot(aes(date_id, conversion_rate, colour = group_id)) +
+p2 <- conv_rate_data %>%
+    ggplot(aes(date_id, conversion_rate, colour = group_id)) +
     geom_point() # + geom_smooth()
+# print(p2)
 
 # add in conf intervals - *check if these need updating for sample population*
 ci_data <- Hmisc::binconf(x = conv_rate_data$conversions, n = conv_rate_data$n, return.df = TRUE)
 conv_rate_data <- bind_cols(conv_rate_data, ci_data)
 
 # plot raw count data with conf intervals
-sql_expanded %>%
-    sample_n(50000) %>%
+p3 <- sql_expanded %>%
+    # sample_n(50000) %>%
     ggplot(aes(date_id, session_result, colour = group_id)) +
-    geom_jitter(alpha = 0.3, position = position_jitterdodge(jitter.height = 0.1)) + 
+    geom_jitter(alpha = 0.15, position = position_jitterdodge(jitter.height = 0.1)) + 
     # geom_smooth() +
     geom_pointrange(
     data = conv_rate_data,
     aes(x = date_id, y = PointEst, ymin = Lower, ymax = Upper, colour = group_id),
     size = 1
-  )
+  ) + xlab("date") + ylab("conversion rate")
+# print(p3)
+# ggsave("conv rate by group.png", p3)
+
+# add in overall values
+sql_expanded_overall <- sql_expanded %>%
+    mutate(date_id = "OVERALL")
+sql_expanded_combined <- sql_expanded %>%
+    mutate(date_id = as.character(date_id)) %>%
+    bind_rows(sql_expanded_overall)
+
+conv_rate_data_overall <- conv_rate_data %>%
+    group_by(group_id) %>%
+    summarise(n = sum(n), conversions = sum(conversions)) %>%
+    mutate(conversion_rate = conversions / n) %>%
+    mutate(date_id = "OVERALL")
+ci_data_overall <- Hmisc::binconf(x = conv_rate_data_overall$conversions, n = conv_rate_data_overall$n, return.df = TRUE)
+conv_rate_data_overall <- bind_cols(conv_rate_data_overall, ci_data_overall)
+conv_rate_data_combined <- bind_rows(
+    mutate(conv_rate_data, date_id = as.character(date_id)), conv_rate_data_overall
+    )
+
+p4 <- sql_expanded_combined %>%
+# sample_n(5000) %>%
+    ggplot(aes(date_id, session_result, colour = group_id)) +
+    geom_jitter(alpha = 0.15, position = position_jitterdodge(jitter.height = 0.1)) + 
+    geom_pointrange(
+    data = conv_rate_data_combined,
+    aes(x = date_id, y = PointEst, ymin = Lower, ymax = Upper, colour = group_id),
+    size = 1
+  ) + xlab("date") + ylab("conversion rate")
+ggsave("conv rate by group.png", p4, width = 15, height = 15)
 
 # weekday effect? Nothing major. Biggest diff is Thurs. BUT only covers 10d so hard to analyse fully.
 conv_rate_data %>%
@@ -50,17 +83,18 @@ conv_rate_data %>%
     mutate(wday(date_id, label = TRUE, abbr = FALSE))
 
 # what about differences in proportions? 
-dfs_list_by_date <- conv_rate_data %>%
+dfs_list_by_date <- conv_rate_data_combined %>%
     group_by(date_id) %>%
     group_split()
 conv_rate_diffs_df <- map_df(dfs_list_by_date, calc_ci_for_diff_of_propns)
 
-conv_rate_diffs_df %>% 
+p5 <- conv_rate_diffs_df %>% 
 ggplot(aes(date_id, point_estimate_diff)) +
     geom_pointrange(
         data = conv_rate_diffs_df,
         aes(x = date_id, y = point_estimate_diff, ymin = lower_ci_diff, ymax = upper_ci_diff),
         size = 1
     ) + 
-    geom_abline(slope = 0, intercept = 0)
-calc_ci_for_diff_of_propns(date_i_df)
+    geom_abline(slope = 0, intercept = 0, colour = "red") + xlab("date") + ylab("difference in conversion rate (A minus B)")
+print(p5)
+ggsave("conv_rate_diffs.png", p5)
