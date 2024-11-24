133dd5e56fd6b092b17ee5048476b313f7077475
jayruffellvw <152837969+jayruffellvw@users.noreply.github.com>
2024-11-24 19:44:47 +1300

    aaaand tweak comments some more.

--- 7_prep-data-for-pbi.r
+++ 7_prep-data-for-pbi.r
@@ -1,5 +1,5 @@
 # This script creates the input csv for the trademe-test.pbix file. The PBI report lets users estimate the value of either strategy A, strategy B, or a "dynamic" strategy that uses a model to dynamically pick either A or B each day based on each strategys' predicted conversion rate. The model won't be perfectly accurate, so users will be able to play around with model accuracy to see how this changes the effectiveness of the dynamic strategy.
-# This script outputs a csv of conversion rate based on each strategy, where conversion rate for "dynamic" is calculated under different scenarios of model accuracy.
+# This script outputs a csv of conversion rate for each strategy, where conversion rate for "dynamic" is calculated under different scenarios of model accuracy. Conversion rates are calculated from the A/B test results provided by Trade Me.
 
 rm(list = ls())
 library(tidyverse)
@@ -7,13 +7,14 @@ source("helpers.r")
 
 aggregated_data <- readr::read_csv("sql_input_202306csv-Sheet1.csv")
 
+# calculate conversion rates from Strategy A and Strategy B, calculated from the A/B test data provided by Trade Me.
 conv_rate_by_strategy <- aggregated_data %>%
     pivot_wider(names_from = session_result, values_from = session_count) %>%
     transmute(date_id, group_id, n = `0` + `1`, conversions = `1`, conversion_rate = `1` / (`0` + `1`)) %>%
     select(date_id, group_id, conversion_rate) %>%
     pivot_wider(names_from = group_id, values_from = conversion_rate)
 
-# calculate conversions based using a "dynamic" strategy, i.e. using a model to predict whether A or B will perform better each day, where model accuracy is either 100%, 50%, or 75%
+# calculate conversion rates based on the "dynamic" strategy, i.e. using a model to predict whether A or B will perform better each day, where model accuracy is either 100%, 50%, or 75%. If accuracy is 100% the conversion rate will always be the greater of Strategy A and Strategy B from the A/B test data, if accuracy is 50% it will be the average of the two, and if it's 75% it will be a weighted average. 
 conv_rate_by_strategy <- conv_rate_by_strategy %>%
     mutate(
         dynamic_100 = pmap_dbl(list(A, B, 1), pick_best_conversion_rate),
